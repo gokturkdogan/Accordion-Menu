f30dbe7a5ba9c0cff08e2b15ebc967bebd377269
gokturkdogan <gokturk.dogan@hotmail.com>
2022-05-29 13:25:32 +0300

    Second Day Finished

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -6,21 +6,15 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("-active");
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
+    if(panel.style.maxHeight){
+      panel.style.maxHeight = null;
     }
+    else{
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+
   });
 }
 
-var acc1 = document.getElementsByClassName("fa-solid");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc1[i].addEventListener("click", function() {
-    this.classList.toggle("-active");
-  });
-}
 
 
